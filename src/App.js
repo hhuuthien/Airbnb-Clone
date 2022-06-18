ac84215bdf15dfcfd7ec0229bf49bb4fb2363e16
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-18 18:32:49 +0700

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
+// setup router
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// pages
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="App">Airbnb</div>;
+  return (
+    <BrowserRouter>
+      {/* <Component cố định /> */}
+      <Switch>
+        <Route exact path={"/home"} component={HomePage} />
+        <Route exact path={"/"} component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
