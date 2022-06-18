027b8b806ce339015d732a984e9c151ea64f0ad6
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-18 18:40:37 +0700

    Config axios

---
+++ src/util/setting.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const DOMAIN = "https://airbnb.cybersoft.edu.vn/";
+export const TOKEN_CYBERSOFT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyMyIsIkhldEhhblN0cmluZyI6IjIwLzEwLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NjIyNDAwMDAwMCIsIm5iZiI6MTYzODExODgwMCwiZXhwIjoxNjY2MzcxNjAwfQ.hoaq9WsA187Q0NvdBYPZsn8c2CRg_ZE4mQO5_lUyAL4";
+export const USER_LOGIN = "userLogin";
+export const ACCESS_TOKEN = "accessToken";
+
+export const http = axios.create({
+  baseURL: DOMAIN,
+  timeout: 10000,
+});
+
+http.interceptors.request.use(
+  (config) => {
+    config.headers = {
+      ...config.headers,
+      ["TokenCybersoft"]: TOKEN_CYBERSOFT,
+      ["Authorization"]: "Bearer " + localStorage.getItem(ACCESS_TOKEN),
+    };
+    return config;
+  },
+  (errors) => {
+    return Promise.reject(errors);
+  }
+);
