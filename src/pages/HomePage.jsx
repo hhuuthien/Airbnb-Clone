05f887748f5350e468ec41c1e4737fea2690bb06
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-20 22:40:03 +0700

    Thêm pagination phân trang cho các location ở trang chủ

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,27 @@
-import { useEffect } from "react";
+import { Pagination } from "antd";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Location from "../components/Location";
 import { getLocationAPI } from "../redux/actions/locationAction";
 
 export default function HomePage() {
   const { locationList } = useSelector((state) => state.locationReducer);
-  console.log("locationList", locationList);
-
   const dispatch = useDispatch();
+  const [current, setCurrent] = useState(1);
+  const [startIndex, setStartIndex] = useState(0);
+  const DEFAULT_PAGE_SIZE = 20;
+
+  const onPaginationChange = (page) => {
+    setCurrent(page);
+    setStartIndex((page - 1) * DEFAULT_PAGE_SIZE);
+  };
 
   const getLocationList = () => {
     dispatch(getLocationAPI());
   };
 
-  const renderLocationList = (locationList) => {
-    const list = locationList.slice(0, 20);
+  const renderLocationList = () => {
+    const list = locationList.slice(startIndex, startIndex + DEFAULT_PAGE_SIZE);
     return list.map((location, index) => {
       return <Location key={index} location={location} />;
     });
@@ -27,7 +34,10 @@ export default function HomePage() {
   return (
     <div className="home-page">
       <div className="container">
-        <div className="location-list">{renderLocationList(locationList)}</div>
+        <div className="location-list">{renderLocationList()}</div>
+        <div className="pagination">
+          <Pagination current={current} onChange={onPaginationChange} total={locationList.length} showSizeChanger={false} defaultPageSize={DEFAULT_PAGE_SIZE} />
+        </div>
       </div>
     </div>
   );
