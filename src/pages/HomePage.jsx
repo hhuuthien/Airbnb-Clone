dc0d2129b62735cf991d6a9d488ec33fe1d32958
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-24 00:21:37 +0700

    Sửa lỗi filter. Kết hợp cả filter và search để có kết quả chính xác hơn

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,31 @@
-import { Button, Input, Pagination, Drawer, Checkbox } from "antd";
+import { Button, Checkbox, Drawer, Input, Pagination } from "antd";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Location from "../components/Location";
 import { getLocationAPI } from "../redux/actions/locationAction";
-import { UPDATE_ARRAY_BY_SEARCHING, UPDATE_ARRAY_BY_FILTERING } from "../redux/const/constant";
+import { UPDATE_ARRAY_BY_FILTERING, UPDATE_ARRAY_BY_SEARCHING } from "../redux/const/constant";
 const { Search } = Input;
 
 export default function HomePage() {
   const { locationList, locationListCopy } = useSelector((state) => state.locationReducer);
   const dispatch = useDispatch();
+
   const [current, setCurrent] = useState(1);
   const [startIndex, setStartIndex] = useState(0);
-  const [check1, setCheck1] = useState(false);
-  const [check2, setCheck2] = useState(false);
-  const [check3, setCheck3] = useState(false);
-  const [check4, setCheck4] = useState(false);
+  const [check1, setCheck1] = useState(true);
+  const [check2, setCheck2] = useState(true);
+  const [check3, setCheck3] = useState(true);
+  const [check4, setCheck4] = useState(true);
+  const [keyword, setKeyword] = useState("");
+  const [visibleDrawer, setVisibleDrawer] = useState(false);
+
   const DEFAULT_PAGE_SIZE = 20;
 
-  const [visibleDrawer, setVisibleDrawer] = useState(false);
   const showDrawer = () => {
     setVisibleDrawer(true);
   };
+
   const onClose = () => {
     setVisibleDrawer(false);
   };
@@ -45,51 +49,64 @@ export default function HomePage() {
     }
   };
 
-  const submitFilter = () => {
-    setVisibleDrawer(false);
+  const locationFilter = (list) => {
     let listFilter1 = [];
     let listFilter2 = [];
     let listFilter3 = [];
     let listFilter4 = [];
     if (check1) {
-      listFilter1 = locationListCopy.filter((location) => location.valueate && location.valueate === 10);
+      listFilter1 = list.filter((location) => location.valueate && location.valueate === 10);
     }
     if (check2) {
-      listFilter2 = locationListCopy.filter((location) => location.valueate && (location.valueate === 9 || location.valueate === 8));
+      listFilter2 = list.filter((location) => location.valueate && (location.valueate === 9 || location.valueate === 8));
     }
     if (check3) {
-      listFilter3 = locationListCopy.filter((location) => location.valueate && (location.valueate === 7 || location.valueate === 6));
+      listFilter3 = list.filter((location) => location.valueate && (location.valueate === 7 || location.valueate === 6));
     }
     if (check4) {
-      listFilter4 = locationListCopy.filter((location) => location.valueate && location.valueate <= 5);
+      listFilter4 = list.filter((location) => location.valueate && location.valueate <= 5);
     }
+    return [...listFilter1, ...listFilter2, ...listFilter3, ...listFilter4];
+  };
+
+  const locationSearch = (list, keyword) => {
+    let listSearch = [];
+    if (keyword === "") {
+      listSearch = [...list];
+    } else {
+      listSearch = list.filter((location) => location.name && location.name.includes(keyword));
+    }
+    return listSearch;
+  };
+
+  const onFilter = () => {
+    setVisibleDrawer(false);
+
+    const listFilterResult = locationFilter(locationListCopy);
+    // Sau khi có được listFilterResult, phải tìm kiếm theo từ khoá hiện đang có
     dispatch({
       type: UPDATE_ARRAY_BY_FILTERING,
-      data: [...listFilter1, ...listFilter2, ...listFilter3, ...listFilter4],
+      data: locationSearch(listFilterResult, keyword),
     });
     setCurrent(1);
     setStartIndex(0);
   };
 
+  const onSearch = (keyword) => {
+    setKeyword(keyword);
+    let listSearchResult = locationSearch(locationListCopy, keyword);
+    // Sau khi có được listSearchResult, phải lọc theo bộ lọc hiện đang được áp dụng
+    dispatch({
+      type: UPDATE_ARRAY_BY_SEARCHING,
+      data: locationFilter(listSearchResult),
+    });
+    setCurrent(1);
+    setStartIndex(0);
+  };
   const onPaginationChange = (page) => {
     setCurrent(page);
     setStartIndex((page - 1) * DEFAULT_PAGE_SIZE);
     window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
-  const onSearch = (value) => {
-    let listSearch = [];
-    if (value === "") {
-      listSearch = [...locationListCopy];
-    } else {
-      listSearch = locationListCopy.filter((location) => location.name && location.name.includes(value));
-    }
-    dispatch({
-      type: UPDATE_ARRAY_BY_SEARCHING,
-      data: listSearch,
-    });
-    setCurrent(1);
-    setStartIndex(0);
   };
 
   const getLocationList = () => {
@@ -102,7 +119,7 @@ export default function HomePage() {
         <motion.div className="no-result" initial={{ x: -200 }} animate={{ x: 0 }}>
           <img src="/img/not-found.png" alt="Không tìm thấy kết quả nào" />
           <p className="nr1">Không tìm thấy kết quả nào</p>
-          <div className="nr2">Hãy thử với từ khoá khác</div>
+          <div className="nr2">Hãy thử tìm kiếm với từ khoá khác hoặc thay đổi bộ lọc kết quả</div>
         </motion.div>
       );
     }
@@ -135,27 +152,27 @@ export default function HomePage() {
           <Drawer title="Bộ lọc kết quả" placement="right" onClose={onClose} visible={visibleDrawer}>
             <div className="cb-area">
               <div className="cb">
-                <Checkbox id="cb1" onChange={onCheckBoxChange}>
+                <Checkbox id="cb1" onChange={onCheckBoxChange} defaultChecked={true}>
                   <i className="fa-regular fa-star"></i> 10
                 </Checkbox>
               </div>
               <div className="cb">
-                <Checkbox id="cb2" onChange={onCheckBoxChange}>
+                <Checkbox id="cb2" onChange={onCheckBoxChange} defaultChecked={true}>
                   <i className="fa-regular fa-star"></i> 8 - 9
                 </Checkbox>
               </div>
               <div className="cb">
-                <Checkbox id="cb3" onChange={onCheckBoxChange}>
+                <Checkbox id="cb3" onChange={onCheckBoxChange} defaultChecked={true}>
                   <i className="fa-regular fa-star"></i> 6 - 7
                 </Checkbox>
               </div>
               <div className="cb">
-                <Checkbox id="cb4" onChange={onCheckBoxChange}>
+                <Checkbox id="cb4" onChange={onCheckBoxChange} defaultChecked={true}>
                   <i className="fa-regular fa-star"></i> 5 và thấp hơn
                 </Checkbox>
               </div>
             </div>
-            <Button type="primary" className="btn-filter-submit" onClick={submitFilter}>
+            <Button type="primary" className="btn-filter-submit" onClick={onFilter}>
               Lọc kết quả
             </Button>
           </Drawer>
