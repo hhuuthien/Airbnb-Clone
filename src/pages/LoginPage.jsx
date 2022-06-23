58b5d86dd998225ff9178ddcb0e5797f7a425dea
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-24 05:11:44 +0700

    Dùng thư viện formik và yub để quản lý form và validation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react";
+import { Button, Input } from "antd";
+import { useFormik } from "formik";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loginAPI } from "../redux/actions/loginAction";
-import { Button, Checkbox, Form, Input } from "antd";
+import * as YUB from "yup";
 
 export default function LoginPage(props) {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: YUB.object({
+      email: YUB.string().required("Không được để trống"),
+      password: YUB.string().required("Không được để trống"),
+    }),
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
-  useEffect(() => {}, []);
-
-  const onInputChange = (e) => {
-    switch (e.target.id) {
-      case "input-email":
-        setEmail(e.target.value);
-        break;
-      case "input-password":
-        setPassword(e.target.value);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const onSubmit = () => {
-    const loginInfo = { email, password };
-    dispatch(loginAPI(loginInfo));
-  };
+  // const onSubmit = () => {
+  //   dispatch(loginAPI(formik.values));
+  // };
 
   return (
     <div className="login-page">
@@ -37,13 +36,15 @@ export default function LoginPage(props) {
             <h2>Đăng nhập</h2>
             <div className="email">
               <label>Email</label>
-              <Input className="input-email" onChange={onInputChange} id="input-email" />
+              <Input className="input-email" value={formik.values.email} onChange={formik.handleChange} name="email" allowClear />
+              {formik.errors.email ? <p className="error">{formik.errors.email}</p> : <></>}
             </div>
             <div className="password">
               <label>Mật khẩu</label>
-              <Input.Password className="input-password" onChange={onInputChange} id="input-password" />
+              <Input.Password className="input-password" value={formik.values.password} onChange={formik.handleChange} name="password" />
+              {formik.errors.password ? <p className="error">{formik.errors.password}</p> : <></>}
             </div>
-            <Button type="primary" className="btn-login" onClick={onSubmit}>
+            <Button type="primary" className="btn-login" onClick={formik.handleSubmit}>
               Đăng nhập
             </Button>
           </div>
